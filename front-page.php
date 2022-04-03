fe44fdf96ff50133ceece3dcfcde63b74b6984ef
saket <aryansaket@yahoo.com>
2022-04-03 13:35:16 +0530

    Work out the hero section

---
+++ front-page.php
@@ -0,0 +1,50 @@
+<?php   get_header(); ?>
+
+<section class="hero h-screen bg-[url('hero-min.jpg')] bg-cover text-white">
+    <div class="navbar h-16  flex justify-between  items-center bg-black bg-opacity-30 px-36">
+                    <div class="text-2xl">
+                       <span> Team</span><span class="font-bold">Colab </span>
+                    </div>
+
+                    <div class="text-base">
+                        <a href="" class="mr-5"> About Us </a>
+                        <a href="" class="mr-5"> Contact Us </a>
+                        <a href="" class="mr-5"> Services </a>
+                        <a href=""> Product </a>
+                    </div>
+
+    </div>
+    <div class="px-36 flex h-full  items-center">
+            <div class="basis-1/2">
+                <div class=" text-5xl font-bold ">
+                    Instant Collaboration for Remote Teams
+                </div>
+                <div class="text-xl font-medium mt-4">
+                        All in one place for your remote team to chat and track progress
+                </div>
+                <div class="mt-12">
+                    <input  class="placeholder:text-gray-800 text-black p-2 rounded-md w-2/3"  placeholder="Provide Email For Early Access.." type="email"  /> 
+                    <button class="bg-brand text-gray-800 py-2 px-3 rounded-md hover:shadow-md hover:shadow-black hover:bg-yellow-600 hover:text-white"> Get Early Access </button>
+                </div>
+            </div>
+            <div class="basis-1/2">
+
+            </div>
+         
+    </div>
+</section>
+
+<section class="px-36">
+        <div class="flex">
+            <div class="basis-1/2 text-center">
+                    <div class="text-5xl font-thin">
+                            Your Hub For TeamWork
+                    </div>
+            </div>  
+            <div class="basis-1/2">
+
+            </div>
+        </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*.php"],
+  theme: {
+    extend: {
+      fontFamily:{
+        cabin: "'Cabin', sans-serif",
+        
+      },
+      colors:{
+        brand: '#FFB200'
+      }
+    }
+  },
+  plugins: [],
+}
